80edc8e904fbb8127fc0c10f5d5949c29b8301a6
Eric Glover <etgt28@mail.missouri.edu>
2017-07-23 00:48:21 -0500

    initial functionality for backend complete

--- app.js
+++ app.js
@@ -1,52 +1,52 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const fs = require("fs");
+const express = require("express");
+const app = express();
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+const port = process.env.PORT || 3000;
+const hostname = "localhost";
 
-var app = express();
+//consider using app.route
 
-const port = process.env.PORT || '3000';
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', index);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.get("/", (req, res) => {
+  //res.writeHead(200, { "Content-Type": "text/html" });
+  //get index.html running
+  console.log(`req.path = ${req.path}`);
+  console.log(`req.url = ${req.url}`);
+  console.log(`req.baseUrl = ${req.baseUrl}`);
+  res.redirect;
+  fs.readFile("./public/index.html", "utf8", function(err, data) {
+    if (err) {
+      res.writeHead(404);
+      res.end("404 Not Found");
+    } else {
+      res.writeHead(200, {
+        "Content-Type": "text/html"
+      });
+      res.end(data);
+    }
+  });
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+//set up blog id's later
+app.get("/blog", (req, res) => {
+  //res.writeHead(200, { "Content-Type": "text/html" });
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  //get handle blog requests
+  fs.readFile("./public/blog.html", "utf8", function(err, data) {
+    if (err) {
+      res.writeHead(404);
+      res.end("404 Not Found");
+    } else {
+      res.writeHead(200, {
+        "Content-Type": "text/html"
+      });
+      res.end(data);
+    }
+  });
 });
+//app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
 
-app.listen( port, function( ){
-  console.log(`Running on port ${port}`)
+app.listen(port, (res, req) => {
+  console.log(`Running on port ${port}`);
 });
-
-module.exports = app;
