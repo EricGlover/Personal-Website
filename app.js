4bfe64d4035db244fbcf3f8e31be910219c98602
Eric Glover <etgt28@mail.missouri.edu>
2017-08-14 01:38:33 -0500

    bare minimum routes

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 const hostname = "localhost";
 
-//consider using app.route
+app.use(express.static(__dirname + "/public"));
+
+const hbs = require("express-handlebars");
+app.engine("handlebars", hbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 app.get("/", (req, res) => {
   //res.writeHead(200, { "Content-Type": "text/html" });
@@ -13,39 +17,40 @@ app.get("/", (req, res) => {
   console.log(`req.path = ${req.path}`);
   console.log(`req.url = ${req.url}`);
   console.log(`req.baseUrl = ${req.baseUrl}`);
-  res.redirect;
-  fs.readFile("./public/index.html", "utf8", function(err, data) {
-    if (err) {
-      res.writeHead(404);
-      res.end("404 Not Found");
-    } else {
-      res.writeHead(200, {
-        "Content-Type": "text/html"
-      });
-      res.end(data);
-    }
-  });
+  res.render("main");
+  // fs.readFile("./public/index.html", "utf8", function(err, data) {
+  //   if (err) {
+  //     res.writeHead(404);
+  //     res.end("404 Not Found");
+  //   } else {
+  //     res.writeHead(200, {
+  //       "Content-Type": "text/html"
+  //     });
+  //     res.end(data);
+  //   }
+  // });
 });
 
 //set up blog id's later
 app.get("/blog", (req, res) => {
-  //res.writeHead(200, { "Content-Type": "text/html" });
-
   //get handle blog requests
-  fs.readFile("./public/blog.html", "utf8", function(err, data) {
-    if (err) {
-      res.writeHead(404);
-      res.end("404 Not Found");
-    } else {
-      res.writeHead(200, {
-        "Content-Type": "text/html"
-      });
-      res.end(data);
-    }
-  });
+  res.render("blog/index");
+  // fs.readFile("./public/blog.html", "utf8", function(err, data) {
+  //   if (err) {
+  //     res.writeHead(404);
+  //     res.end("404 Not Found");
+  //   } else {
+  //     res.writeHead(200, {
+  //       "Content-Type": "text/html"
+  //     });
+  //     res.end(data);
+  //   }
+  // });
 });
-//app.use(express.static("public"));
-app.use(express.static(__dirname + "/public"));
+app.get("/blog/:id", (req, res) => {
+  //get handle blog requests
+  res.render("blog/" + req.params.id);
+});
 
 app.listen(port, (res, req) => {
   console.log(`Running on port ${port}`);
