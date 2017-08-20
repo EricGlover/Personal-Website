c77649b17f9f0f914b4f6c1de08cfdb679760263
Eric Glover <etgt28@mail.missouri.edu>
2017-08-19 22:01:08 -0500

    bootstrappin in progress

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-const fs = require("fs");
 const express = require("express");
 const app = express();
 const logger = require("morgan");
@@ -6,20 +5,20 @@ const logger = require("morgan");
 const port = process.env.PORT || 3000;
 const hostname = "localhost";
 
-app.use(express.static(__dirname + "/public"));
-
 //handlebars
 const hbs = require("express-handlebars");
 app.engine("handlebars", hbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// const { indexRouter, blogRouter, projectRouter } = require("./routers");
+const { indexRouter, blogRouter, projectRouter } = require("./routers");
 
-// app.use(logger("dev"));
-// app.use("/blog", blogRouter);
-// app.use("/", indexRouter);
-// app.use("/projects", projectRouter);
+app.use(logger("dev"));
+app.use("/blog", blogRouter);
+app.use("/", indexRouter);
+app.use("/projects", projectRouter);
+
+app.use(express.static(__dirname + "/public"));
 
 app.listen(port, (res, req) => {
-  console.log(`Running on port hkjhkjh${port}`);
+  console.log(`Running on port ${port}`);
 });
--- routers/index.js
+++ routers/index.js
@@ -2,8 +2,7 @@ const express = require("express");
 let router = express.Router();
 
 router.get("/", (req, res) => {
-  // res.render("main");
-  res.end("things");
+  res.render("main");
 });
 
 module.exports = {
