9c79b8ce715f3a5a85078bb16a514d330da519f0
Eric Glover <etgt28@mail.missouri.edu>
2017-09-04 14:40:33 -0500

    switching to master

---
+++ newUrl.js
@@ -0,0 +1,3 @@
+const md5 = require("md5");
+let stuff = "Quick HTML CSS Tip 7/16/17";
+console.log(md5(stuff));
--- routers/blog.js
+++ routers/blog.js
@@ -1,6 +1,15 @@
 const express = require("express");
 let router = express.Router();
 
+//legacy support
+let urls = {
+  "47edaa7269d266e037079567291f0dda": 1,
+  2: ["2"],
+  3: ["3"],
+  4: ["4"],
+  5: ["5"]
+};
+
 //set up blog id's later
 router.get("/", (req, res) => {
   //get handle blog requests
@@ -8,7 +17,9 @@ router.get("/", (req, res) => {
 });
 router.get("/:id", (req, res) => {
   //get handle blog requests
-  res.render("blog/" + req.params.id, { blog: true });
+  //get number from url
+  let blogNum = urls[req.params.id] || req.params.id;
+  res.render("blog/" + blogNum, { blog: true });
 });
 
 module.exports = router;
