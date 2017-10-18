9fdd5906e87f221e1b357d5d5d985aa48e4738e7
Eric Glover <etgt28@mail.missouri.edu>
2017-10-18 17:46:59 -0500

    cleaned up the gallery

--- routers/projects.js
+++ routers/projects.js
@@ -2,10 +2,10 @@ const express = require("express");
 let router = express.Router();
 
 router.get("/", (req, res) => {
-  return res.render("underConstruction");
+  return res.render("projects/portfolio");
 });
 router.get("/lca", (req, res) => {
-  res.render("testFlash");
+  res.render("projects/lca");
 });
 
 module.exports = router;
