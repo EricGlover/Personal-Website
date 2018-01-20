c159995ca8743c14c50e2cfea89d130c94cb69a3
Eric Glover <etgt28@mail.missouri.edu>
2018-01-19 21:43:35 -0600

    commencing mandelbrot experiments

--- scrap.js
+++ scrap.js
@@ -1,6 +1,6 @@
 //the idea hear is we test using the canvas api
 //to draw the mandelbrot set
-
+const depth = 100; //all cases pass at 100 for sure
 //
 // const complex = function(){
 //   return {
@@ -31,11 +31,11 @@ const isMandelbrot = (cR, cI) => {
   };
 
   let z = [0, 0];
-  maxIterations = 100;
+  maxIterations = depth;
   distance = 2 * 2;
   for (let i = 0; i < maxIterations; i++) {
     z = fn(...z, cR, cI);
-    console.log(`iteration ${i}: z is now ${z}`);
+    // console.log(`iteration ${i}: z is now ${z}`);
     if (z[0] * z[0] + z[1] * z[1] > distance) {
       return false;
     }
@@ -43,6 +43,8 @@ const isMandelbrot = (cR, cI) => {
   return true;
 };
 
+//TODO: setup some tests for pixels scaling
+//TODO: add some benchmarking
 //quick and dirty test function
 const test = () => {
   //isMandelbrot test
@@ -51,48 +53,109 @@ const test = () => {
     //basic values of z that are in the set
     {
       z: [0, 0],
-      answer: true
+      answer: true,
+      failed: true
     },
     {
       z: [-1, 0],
-      answer: true
+      answer: true,
+      failed: true
     },
     {
       z: [0, 1],
-      answer: true
+      answer: true,
+      failed: true
     },
     {
-      z: [0, -2],
-      answer: true
+      z: [-2, 0],
+      answer: true,
+      failed: true
     },
     //basic values of z that are not in the set, the corners
     {
       z: [-2, 1],
-      answer: false
+      answer: false,
+      failed: true
     },
     {
       z: [1, 1],
-      answer: false
+      answer: false,
+      failed: true
     },
     {
       z: [1, -1],
-      answer: false
+      answer: false,
+      failed: true
     },
     {
       z: [-2, -1],
-      answer: false
+      answer: false,
+      failed: true
+    },
+    //testing floats
+    {
+      z: [-0.2, -0.1],
+      answer: true,
+      failed: true
+    },
+    {
+      z: [0, 0.1],
+      answer: true,
+      failed: true
+    },
+    {
+      z: [-1, 0.0001],
+      answer: true,
+      failed: true
+    },
+    {
+      z: [-0.2, 0.2],
+      answer: true,
+      failed: true
+    },
+    //zooming in and starting to look for detail
+    {
+      z: [-0.75, 0],
+      answer: true,
+      failed: true
+    },
+    {
+      z: [-0.784, 0.344],
+      answer: false,
+      failed: true
+    },
+    {
+      z: [-0.784, 0.275],
+      answer: false,
+      failed: true
+    },
+    {
+      z: [-0.767, 0.189],
+      answer: false,
+      failed: true
+    },
+    {
+      z: [-0.802, 0.12],
+      answer: true,
+      failed: true
     }
   ];
   let failedCases = 0;
-  tests.forEach(t => {
+  tests.forEach((t, i) => {
     if ((ans = isMandelbrot(...t.z)) != t.answer) {
       console.error(
-        `case : ${t.z}, isMandelbrot() -> ${ans}, expected ${t.answer}`
+        `case : ${i}, isMandelbrot(${t.z}) -> ${ans}, expected ${t.answer}`
       );
       failedCases++;
+    } else {
+      t.failed = false;
     }
   });
+  tests.forEach(printOverview);
   console.log(`failed ${failedCases} cases`);
 };
-test();
 
+const printOverview = (c, i) =>
+  console.log(`case ${i}: ${c.failed ? "FAIL" : "Pass"}`);
+
+test();
